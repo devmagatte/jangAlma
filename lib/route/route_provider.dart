f954ad702b3c473557cf62e5d54715e7cd8c3db4
Magatte Diallo <squadren2@gmail.com>
2021-09-27 13:04:32 +0000

    route create

---
+++ lib/route/route_provider.dart
@@ -0,0 +1,34 @@
+import 'package:cours_allemand/ui/home/home_view.dart';
+import 'package:flutter/material.dart';
+import 'routeName.dart';
+
+class RouteGeerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '$initRoute':
+        return MaterialPageRoute(
+          builder: (_) => HomeView(
+            title: args.toString(),
+          ),
+        );
+
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Error'),
+        ),
+        body: Center(
+          child: Text('Route not found'),
+        ),
+      );
+    });
+  }
+}
---
+++ lib/ui/home/home_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  final String title;
+  HomeView({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
